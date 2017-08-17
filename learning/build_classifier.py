eb4926749ca3ea6faece834725693dd91076395f
Markus Stocker <markus.stocker@gmail.com>
2017-08-17 18:45:54 +0200

    working on classifier

--- learning/build_classifier.py
+++ learning/build_classifier.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 import numpy as np
+import os.path
 from datetime import date, timedelta
 from learning.featurizer import featurize
 from smear.datareader import readdata
@@ -10,9 +11,10 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import MinMaxScaler
 
 location = 'hyytiaelae'
-start_day = date(2013, 1, 1)
+start_day = date(2010, 1, 1)
 end_day = date(2013, 12, 31)
 current_day = start_day
 example_day = date(2013, 4, 4)
@@ -25,63 +27,110 @@ X_example = []
 labels = ['Class Ia', 'Class Ib', 'Class II']
 labels = ['Event', 'Non Event']
 
-# Create labelled dataset
-while current_day <= end_day:
-    current_day_str = current_day.strftime('%Y-%m-%d')
 
+def svd_vector(data):
+    svd = TruncatedSVD(n_components=1)
+    vector = svd.fit_transform(data.ix[:, 6:].transpose())
+    return [item for sublist in vector for item in sublist]
+
+
+def feature_vector(data):
+#    data_day = data.ix[:, 6:].as_matrix()
+#    data_daytime = data.loc[(data['Hour'] >= 9) & (data['Hour'] <= 15)].ix[:, 6:].as_matrix()
+    data_day_new = data.ix[:, 6:16].as_matrix()  # Number concentration of particles with diameter <= 7nm (13) 15nm (20)
+    data_daytime_new = data.loc[(data['Hour'] >= 9) & (data['Hour'] <= 15)].ix[:, 6:20].as_matrix()
+    data_nighttime_new = data.loc[(data['Hour'] < 9) | (data['Hour'] > 15)].ix[:, 6:20].as_matrix()
+    ret = [
+        np.sum(data_day_new),
+        np.max(data_day_new),
+        np.min(data_day_new),
+        np.var(data_day_new),
+#        np.sum(data_nighttime_new),
+#        np.max(data_nighttime_new),
+#        np.min(data_nighttime_new),
+#        np.var(data_nighttime_new),
+#        np.sum(data_daytime_new) - np.sum(data_nighttime_new),
+#        np.max(data_daytime_new) - np.max(data_nighttime_new),
+#        np.min(data_daytime_new) - np.min(data_nighttime_new),
+#        np.var(data_daytime_new) - np.var(data_nighttime_new)
+    ]
+    return ret
+
+if __name__ == "__main__":
     cls_data_file = '{}/classification/{}_dmps_event_classification_{}.txt'.format(dir, location, cls_period)
     cls_data = pd.read_csv(cls_data_file, sep=' ', header=None, names=['Matlab Datenum', 'Class Ia', 'Class Ib',
                                                                        'Class II', 'N/A', 'N/A', 'N/A', 'N/A',
                                                                        'Non Event', 'Undefined', 'Bad Data',
                                                                        'Partly Bad Data', 'Checksum'])
 
-    date_num = int(date2datenum(current_day_str))
-    cls_data_day = cls_data.loc[cls_data['Matlab Datenum'] == date_num]
+    # Create labelled dataset
+    while current_day <= end_day:
+        current_day_str = current_day.strftime('%Y-%m-%d')
 
-    if cls_data_day.iloc[0]['Class Ia'] == 1:
-        label = 'Event'
-    elif cls_data_day.iloc[0]['Class Ib'] == 1:
-        label = 'Event'
-    elif cls_data_day.iloc[0]['Class II'] == 1:
-        label = 'Event'
-    elif cls_data_day.iloc[0]['Non Event'] == 1:
-        label = 'Non Event'
-    else:
-        print('Skip {} (class)'.format(current_day_str))
+        print('Processing day {}'.format(current_day_str))
+
+        date_num = int(date2datenum(current_day_str))
+        cls_data_day = cls_data.loc[cls_data['Matlab Datenum'] == date_num]
+
+        if cls_data_day.iloc[0]['Class Ia'] == 1:
+            label = 'Event'
+        elif cls_data_day.iloc[0]['Class Ib'] == 1:
+            label = 'Event'
+        elif cls_data_day.iloc[0]['Class II'] == 1:
+            label = 'Event'
+        elif cls_data_day.iloc[0]['Non Event'] == 1:
+            label = 'Non Event'
+        else:
+            print('Skip {} (unsupported class)'.format(current_day_str))
+            current_day = current_day + timedelta(days=1)
+            continue
+
+        obs_data_file = '{}/observational/{}/{}_{}.csv'.format(dir, location, location, current_day_str)
+
+        if not os.path.isfile(obs_data_file):
+            print('Skip {} (file not found)'.format(current_day_str))
+            current_day = current_day + timedelta(days=1)
+            continue
+
+        obs_data = pd.read_csv(obs_data_file)
+
+        if obs_data.isnull().values.any():
+            print('Skip {} (is null)'.format(current_day_str))
+            current_day = current_day + timedelta(days=1)
+            continue
+
+        if len(obs_data) == 0:
+            print('Skip {} (no data)'.format(current_day_str))
+            current_day = current_day + timedelta(days=1)
+            continue
+
+        vector = feature_vector(obs_data)
+
+        if current_day == example_day:
+            X_example = vector
+
+        X.append(vector)
+        Y.append(label)
+
         current_day = current_day + timedelta(days=1)
-        continue
 
-    obs_data_file = '{}/observational/{}/{}_{}.csv'.format(dir, location, location, current_day_str)
-    obs_data = pd.read_csv(obs_data_file)
+    print('Number of samples: {}'.format(len(X)))
 
-    # obs_data = obs_data.loc[(obs_data['Hour'] >= 6) & (obs_data['Hour'] <= 18)]
+    scaler = MinMaxScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
 
-    if obs_data.isnull().values.any():
-        print('Skip {} (is null)'.format(current_day_str))
-        current_day = current_day + timedelta(days=1)
-        continue
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=100)
 
-    svd = TruncatedSVD(n_components=1)
-    svd_data = svd.fit_transform(obs_data.ix[:, 6:].transpose())
-    svd_data = [item for sublist in svd_data for item in sublist]
+    mlp = MLPClassifier(solver='lbfgs', hidden_layer_sizes=[2], max_iter=2000, activation='logistic')
+    mlp.fit(X_train, Y_train)
 
-    if current_day == example_day:
-        X_example = svd_data
+    Y_pred = mlp.predict(X_test)
 
-    X.append(svd_data)
-    Y.append(label)
+    print(accuracy_score(Y_test, Y_pred) * 100)
+    print(confusion_matrix(Y_test, Y_pred, labels=labels))
+    print(classification_report(Y_test, Y_pred, target_names=labels))
 
-    current_day = current_day + timedelta(days=1)
+    print(mlp.predict([X_example]))
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=100)
 
-mlp = MLPClassifier()
-mlp.fit(X_train, Y_train)
-
-Y_pred = mlp.predict(X_test)
-
-print(accuracy_score(Y_test, Y_pred)*100)
-print(confusion_matrix(Y_test, Y_pred, labels=labels))
-print(classification_report(Y_test, Y_pred, target_names=labels))
-
-print(mlp.predict([X_example]))
--- smear/datacollector.py
+++ smear/datacollector.py
@@ -2,16 +2,25 @@ from smear.datafetcher import fetchdata
 from smear.dataplotter import plotdata
 from smear.datawriter import writedata
 from smear.datareader import readdata
+from datetime import date, timedelta
+from time import sleep
 
-date = '2013-12-31'
 
-place = 'Värriö' # Hyytiälä, Puijo, Värriö
-placename = 'vaerrioe' # hyytiaelae, puijo, vaerrioe
+start_day = date(2010, 1, 1)
+end_day = date(2010, 12, 31)
+current_day = start_day
 
-filename = '{}_{}.csv'.format(placename, date)
-file = '/home/ms/workspace-pynpf/pynpf-data/observational/{}/{}'.format(placename, filename)
+place = 'Hyytiälä' # Hyytiälä, Puijo, Värriö
+placename = 'hyytiaelae' # hyytiaelae, puijo, vaerrioe
 
-#writedata(fetchdata(date, place), file)
-#plotdata(readdata(file))
+while current_day <= end_day:
+    filename = '{}_{}.csv'.format(placename, current_day.strftime('%Y-%m-%d'))
+    file = '/home/ms/workspace-pynpf/pynpf-data/observational/{}/{}'.format(placename, filename)
+
+    writedata(fetchdata(current_day.strftime('%Y-%m-%d'), place), file)
+    current_day = current_day + timedelta(days=1)
+
+    sleep(1)
+    #plotdata(readdata(file))
 
 
